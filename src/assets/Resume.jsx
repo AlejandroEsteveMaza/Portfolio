6241e7fa69d49a94bb78192adf9f9335ab061608
Alejandro Esteve Maza <alex.esteve98@gmail.com>
2023-10-01 16:21:42 +0200

    v.1.2

--- src/assets/Resume.jsx
+++ src/assets/Resume.jsx
@@ -2,14 +2,20 @@ import "./Resume.css";
 import React from 'react';
 
 function Resume() {
+    const pdfUrl = '/AlejandroEsteveMazaCV.pdf';
+    const downloadResume = () => {
+        window.open(pdfUrl, '_blank');
+    };
+
     return (
         <section id="resume" className="Resume-container">
             <div className="Resume-title">
                 <h2>Resume</h2>
             </div>
             <div className="Resume">
-            <iframe src="/aemCV.pdf"></iframe>
+                <iframe src="https://docs.google.com/document/d/1oKdDaLbbBmsTXKk-raCG5xBw1JbLD0et/preview"></iframe>
             </div>
+            <button  className="btn-download" onClick={downloadResume}>Download PDF</button>
         </section>
     );
 }
